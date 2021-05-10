4cacc11701de7fa66b3cce9e3d32ffecfdb07357
Mark Patton <mark.patton@gmail.com>
2021-05-10 15:49:52 -0400

    Add constants for created/modified Fedora headers and Elasticsearch fields.

--- pass-indexer-core/src/main/java/org/dataconservancy/pass/indexer/IndexerConstants.java
+++ pass-indexer-core/src/main/java/org/dataconservancy/pass/indexer/IndexerConstants.java
@@ -8,6 +8,11 @@ import okhttp3.MediaType;
 public interface IndexerConstants {
     String FEDORA_ACCEPT_HEADER = "application/ld+json; profile=\"http://www.w3.org/ns/json-ld#compacted\"";
     String FEDORA_PREFER_HEADER = "return=representation; omit=\"http://fedora.info/definitions/v4/repository#ServerManaged\"";
+    String FEDORA_CREATED_HEADER = "X-CREATED";
+    String FEDORA_MODIFIED_HEADER = "X-MODIFIED";
+   
+    String CREATED_FIELD = "fcrepo_created";
+    String MODIFIED_FIELD = "fcrepo_modified";
     
     MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 }
